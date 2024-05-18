bcdde046647994e36d7fa20a05ff381b9143c376
polgr <polgr@MSI>
2024-05-18 17:45:00 +0200

    Agregar archivos de proyecto.

---
+++ AA1-Encriptar-PolGranadoGali/function_encript.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void menuSeleccion(){
+
+	cout << "Bienvenido elige una de las opciones:"
+	cout << "1--> Encriptar"
+
+
+
+}
